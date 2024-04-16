3cb8171d9d715eb8bc535d1b6ec8a9422a0761ff
Cao Quynh An <ancaoquynh@gmail.com>
2024-04-16 22:18:38 +0200

    factory update

--- Factory/Factory/Infile.cs
+++ Factory/Factory/Infile.cs
@@ -2,18 +2,87 @@
 using TextFile;
 namespace Factory
 {
-	struct Worker
+
+	public class Worker
 	{
-		
+		public int id;
+		public int product;
+
+
+		Worker(int id, int product)
+		{
+			this.id = id;
+			this.product = product;
+		}
+
+		public static void Read(ref Infile.Status st, ref Worker e, ref TextFileReader x)
+		{
+			e = new Worker(0, 0);
+			bool l = x.ReadInt(out e.id) && x.ReadInt(out e.product);
+			st = l ? Infile.Status.norm : Infile.Status.abnorm;
+		}
 	}
+	public class Output
+	{
+		public int id;
+		public int maxPr;
+        public override string ToString()
+        {
+			return "(" + id + "," + maxPr + ")";
+        }
+    }
 
 
 	public class Infile
 	{
-
-
-		public Infile()
+		public enum Status { norm, abnorm};
+		private Status st;
+		private Worker e;
+		private Output curr;
+		private TextFileReader x;
+		private bool end;
+		
+		public Infile( string filename)
 		{
+			x = new TextFileReader(filename);
+			curr = new Output();
+		}
+
+
+		public bool End()
+		{
+			return end;
+		}
+
+		public Output Current()
+		{
+			return curr;
+		}
+
+
+		public void First()
+		{
+			Worker.Read(ref st, ref e, ref x);
+			Next();
+
+		}
+
+		public void Next()
+		{
+			end = (st == Status.abnorm);
+			if (!end)
+			{
+				curr.id = e.id;
+				curr.maxPr = 0;
+				while (e.id == curr.id && st == Status.norm)
+				{
+					if (e.product > curr.maxPr)
+					{
+						curr.maxPr = e.product;
+					}
+					Worker.Read(ref st, ref e, ref x);
+				}
+			}
 		}
 	}
 }
--- Factory/Factory/Program.cs
+++ Factory/Factory/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Factory;
+﻿using TextFile;
+namespace Factory;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool FileErr = true;
+        do
+        {
+            try
+            {
+                Console.WriteLine("Enter file name: ");
+                string name = Console.ReadLine();
+                Infile t = new Infile(name);
+
+                t.First();
+
+                List<string> list = new List<string>();
+                while (!t.End())
+                {
+
+                    list.Add(t.Current().ToString());
+                    t.Next();
+                }
+
+                string a = String.Join(",", list);
+                Console.WriteLine(a);
+
+
+
+                FileErr = false;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+        } while (FileErr);
     }
 }
 
